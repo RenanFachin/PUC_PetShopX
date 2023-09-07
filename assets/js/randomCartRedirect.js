9a9c1b66cee249bea94515b9e5b2deb943ddfce3
Renan <renan.fachin@hotmail.com>
2023-09-06 23:11:39 -0300

    fix: cart links

--- assets/js/randomCartRedirect.js
+++ assets/js/randomCartRedirect.js
@@ -3,7 +3,7 @@ function redirectToRandomPage() {
   const randomNumber = Math.random()
 
   const cartPageWithProductsAdded = 'https://renanfachin.github.io/PUC_PetShopX/pages/cart.html'
-  const emptyCartPage = 'https://renanfachin.github.io/PUC_PetShopX/emptyCart.html'
+  const emptyCartPage = 'https://renanfachin.github.io/PUC_PetShopX/pages/emptyCart.html'
 
   // Utilizando operador ternário para decidir para qual página o usuário será direcionado
   // isto só está sendo criado para a entrega do projeto, em produção isto não seria necessário.
